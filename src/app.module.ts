6c494a8cd51c237a149904b663f13bd1bfc24e51
Estêvão Fáy <eff.fay@gmail.com>
2025-05-03 17:58:08 -0300

    feat(pino-pretty): use pino pretty

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,16 @@ import { AppService } from './app.service';
 import { LoggerModule } from 'nestjs-pino';
 
 @Module({
-  imports: [LoggerModule.forRoot()],
+  imports: [LoggerModule.forRoot({
+    pinoHttp:{
+      transport: {
+        target: 'pino-pretty',
+        options: {
+          singleLine: true,
+        }
+      }
+    }
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
