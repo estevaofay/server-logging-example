0f7b0388b1dcf6230923167a2ece568daa4f4621
Estêvão Francisco Fáy <eff.fay@gmail.com>
2025-05-03 19:07:14 -0300

    feat(pino): configure nestjs pino (#3)
    * build(pino-pretty): install pino pretty
    
    * feat(pino-pretty): use pino pretty
    
    * feat(pino-pretty): enable pino-pretty based on .env configuration
    
    * feat(pino): use randomly generated request id
    
    * feat(pino): log entire request.body
    
    * feat(pino): add redaction capability
    
    * feat(pino): add simple exclusion rules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,32 @@
-import { Module } from '@nestjs/common';
+import { Module, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggerModule } from 'nestjs-pino';
+import { randomBytes } from 'crypto';
 
 @Module({
-  imports: [LoggerModule.forRoot()],
+  imports: [LoggerModule.forRoot({
+    pinoHttp:{
+      genReqId: () =>
+        randomBytes(12).toString('hex'),
+      transport: process.env.ENABLE_PINO_PRETTY === 'true'
+        ? {
+          target: 'pino-pretty',
+          options: {
+            singleLine: true,
+          },
+        }
+        : undefined,
+      serializers: {
+        req(req) {
+          req.body = req.raw.body;
+          return req;
+        },
+      },
+      redact: ["hostname"],
+    },
+    exclude: [{ method: RequestMethod.ALL, path: 'health' }],
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
