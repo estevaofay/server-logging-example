1dbc4f1608ba145b296f2a693587e3496816c649
Estêvão Fáy <eff.fay@gmail.com>
2025-05-03 18:07:40 -0300

    feat(pino): use randomly generated request id

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggerModule } from 'nestjs-pino';
+import { randomBytes } from 'crypto';
 
 @Module({
   imports: [LoggerModule.forRoot({
     pinoHttp:{
+      genReqId: () =>
+        randomBytes(12).toString('hex'),
       transport: process.env.ENABLE_PINO_PRETTY === 'true'
         ? {
           target: 'pino-pretty',
