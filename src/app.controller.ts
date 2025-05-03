3b6b6f761bbd9a0baf70f4d133368b05dde2cc62
Estêvão Francisco Fáy <eff.fay@gmail.com>
2025-05-03 18:59:51 -0300

    feat(controllers): add sample controllers (#1)
    * feat(health): add health controller
    
    * feat(error): add error endpoint
    
    * feat(complex-endpoint): modify service and controller to simulate a complex endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('/health')
+  @HttpCode(HttpStatus.OK)
+  health(): string {
+    return 'OK';
+  }
+
+  @Get('/error')
+  @HttpCode(HttpStatus.INTERNAL_SERVER_ERROR)
+  error(): string {
+    throw new Error('Internal Server Error');
+  }
+
+  @Get('/complex')
+  complexEndpoint() {
+    return this.appService.complexEndpoint();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,47 @@
 import { Injectable } from '@nestjs/common';
+const { randomInt } = require('crypto');
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  async complexEndpoint() {
+
+    const [dbResult, apiResult] = await Promise.all([this.simulateDbCall(), this.simulateApiCall(), this.logIteratively])
+    return {dbResult, apiResult};
+  }
+
+  simulateDbCall() {
+    const delay = randomInt(100, 300); // Random DB delay
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        console.log('[DB] Fetched data in', delay, 'ms');
+        resolve({ data: 'db-result' });
+      }, delay);
+    });
+  }
+
+  simulateApiCall() {
+    const delay = randomInt(200, 400); // Random API delay
+    const shouldFail = Math.random() < 0.5;
+
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        if (shouldFail) {
+          console.log('[API] Failed after', delay, 'ms');
+          resolve({data: "API error" });
+        } else {
+          console.log('[API] Success after', delay, 'ms');
+          resolve({ data: 'API success' });
+        }
+      }, delay);
+    });
+  }
+  sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
+  async logIteratively() {
+    for (let i = 1; i <= 5; i++) {
+      await this.sleep(200); // Wait 200ms
+      console.log(`[Iterative Logic] Step ${i}`);
+    }
   }
 }
