e752a2b57c44b5c63df507d74052138b58098ddf
Estêvão Fáy <eff.fay@gmail.com>
2025-05-03 19:23:32 -0300

    fix(log-middleware): skip logs for /health endpoint

--- src/logger.middleware.ts
+++ src/logger.middleware.ts
@@ -4,7 +4,10 @@ import { Request, Response, NextFunction } from 'express';
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
   private logger = new Logger(LoggerMiddleware.name);
-  use(_req: Request, _res: Response, next: NextFunction) {
+  use(req: Request, _res: Response, next: NextFunction) {
+    if (req.url.startsWith('/health')) {
+      return next();
+    }
     this.logger.log("Request received")
     return next();
   }
